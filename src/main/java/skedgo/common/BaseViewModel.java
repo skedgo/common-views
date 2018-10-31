c77cf461d54b1a20a30e98ed889f8a73cabdc284
Emmanuel Lagarrigue Lazarte <emmanuel.lagarrigue@black-tobacco.com>
2018-10-31 08:47:29 -0300

    Migrate to androidx (#8)
    * Set compile sdk v  28
    
    * Update android tools version
    
    * Migrate to androidx
    
    * Set target 28
    
    * Use testImplementation
    
    * Revert to 27
    
    * Add CI config
    
    * Trigger build
    
    * Upgrade gradle
    
    * Add plugin
    
    * Do not run this
    
    * Remove it
    
    * Put local Maven
    
    * Use Android X
    
    * Use 3.2.0

--- src/main/java/skedgo/common/BaseViewModel.java
+++ src/main/java/skedgo/common/BaseViewModel.java
@@ -1,11 +1,11 @@
 package skedgo.common;
 
 import android.os.Bundle;
-import android.support.annotation.CallSuper;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
+import androidx.annotation.CallSuper;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
+import androidx.fragment.app.Fragment;
 import rx.Observable;
 import rx.subjects.PublishSubject;
 
